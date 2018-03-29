b6e122c9f34a826914c1276311da7084ce4efe33
Romulo Coutinho <romulocoutinho16@gmail.com>
2018-03-29 17:33:45 +0000

    exe2

--- exercises/2-linked lists/lib/list.hpp
+++ exercises/2-linked lists/lib/list.hpp
@@ -16,11 +16,16 @@ class list
 public:
 	list(){
 		_tail = new node<T>;
+		_tail->next = _tail;
+		
 		_size = 0;
 	};
 	
 	~list(){
-		
+		while(_size > 0){
+			pop_back();
+		}
+		delete _tail;
 	};
 	
 	unsigned int size(){
@@ -32,51 +37,137 @@ public:
 	};
 	
 	T value_at(unsigned int index){
-		
-	};
-	
-	void push_front(T value){
-		
-	};
-	
-	T pop_front(){
-		
+		node<T>* aux = _tail->next;
+		for(int i=0; i<=index; ++i){
+			aux = aux->next;
+		}
+		return aux->value;
 	};
 	
 	void push_back(T value){
-		
+		node<T>* back = new node<T>;
+		back->next = _tail->next;
+		back->value = value;
+		_tail->next = back;
+		++_size;
 	};
 	
 	T pop_back(){
-		
+		node<T>* aux = _tail->next->next;
+		T value = _tail->next->value;
+		delete _tail->next;
+		_tail->next = aux;
+		--_size;
+		return value;
+	};
+	
+	void push_front(T value){
+		node<T>* aux = _tail->next;
+		while(aux->next != _tail){
+			aux = aux->next;
+		}
+		node<T>* front = new node<T>;
+		front->next = aux->next;
+		front->value = value;
+		aux->next = front;
+		++_size;
+	};
+	
+	T pop_front(){
+		node<T>* last = _tail;
+		node<T>* aux = _tail->next;
+		while(aux->next != _tail){
+			aux = aux->next;
+			last = last->next;
+		}
+		T value = aux->value;
+		last->next = aux->next;
+		delete aux;
+		--_size;
+		return value;
 	};
 	
 	T front(){
-		
+		node<T>* aux = _tail->next;
+		while(aux->next != _tail){
+			aux = aux->next;
+		}
+		return aux->value;
 	};
 	
 	T back(){
-		
+		return _tail->next->value;
 	};
 	
 	void insert(unsigned int index, T value){
-		
+		node<T>* aux = _tail;
+		for(int i=0; i<index; ++i){
+			aux = aux->next;
+		}
+		node<T>* insert = new node<T>;
+		insert->value = value;
+		insert->next = aux->next;
+		aux->next = insert;
+		++_size;
 	};
 	
-	void erase(unsigned int index){
-		
+	T erase(unsigned int index){
+		node<T>* last = _tail;
+		node<T>* aux = _tail->next;
+		for(int i=0; i<index; ++i){
+			aux = aux->next;
+			last = last->next;
+		}
+		T value = aux->value;
+		last->next = aux->next;
+		delete aux;
+		--_size;
+		return value;
 	};
 	
 	T value_of_n(unsigned int n){
-		
+		node<T>* aux = _tail->next;
+		for(int i=0; i<_size-n-1; ++i){
+			aux = aux->next;
+		}
+		return aux->value;
 	};
 	
+	//invert all next pointers
 	void reverse(){
-		
+		node<T>* last_aux = _tail;
+		node<T>* aux = _tail->next;
+		node<T>* next_aux = aux->next;
+		for(int i=0; i<_size+1; ++i){
+			aux->next = last_aux;
+			last_aux = aux;
+			aux = next_aux;
+			next_aux = next_aux->next;
+		}
 	};
 	
-	unsigned int remove_value(T value){
-		
+    void remove_value(T value){
+		node<T>* last = _tail;
+		node<T>* aux = _tail->next;
+		while(aux->value != value){
+			last = last->next;
+			aux = aux->next;
+			if(aux == _tail){
+				return;				
+			}
+		}
+		last->next = aux->next;
+		delete aux;
+		--_size;
+	};
+	
+	void print(){
+		node<T>* aux = _tail->next;
+		do{
+			std::cout << aux->value << " ";
+			aux = aux->next;
+		}while(aux != _tail);
+		std::cout << "\n";
 	};
 
 private:
--- exercises/2-linked lists/src/main.cpp
+++ exercises/2-linked lists/src/main.cpp
@@ -3,7 +3,41 @@
 #include "../lib/list.hpp"
 
 int main(){
-    node<int>* a = new node<int>;
-    a->value = 1;
-    std::cout << a->value;
+    list<int>* a = new list<int>();
+    for(int i=1; i<=100000; ++i){
+        a->push_back(i);
+    }
+    a->push_back(13);
+    a->push_front(13);
+    a->print();
+    std::cout << a->pop_back() << "\n";
+    std::cout << a->pop_front() << "\n";
+    std::cout << a->back() << "\n";
+    std::cout << a->front() << "\n";
+    a->print();
+    
+    a->insert(0, 13);
+    a->print();
+    std::cout << a->erase(0) << std::endl;
+    std::cout << a->erase(9) << std::endl;
+    std::cout << a->erase(3) << std::endl;
+    a->print();
+    std::cout << a->value_of_n(0) << std::endl;
+    std::cout << a->value_of_n(3) << std::endl;
+    a->remove_value(9);
+    a->print();
+    a->reverse();
+    a->print();
+    
+    list<int>* b = new list<int>();
+    b->push_back(1);
+    b->push_back(2);
+    std::cout << "B1: ";
+    b->print();
+    b->reverse();
+    std::cout << "B2: ";
+    b->print();
+    delete a;
+    delete b;
 }
+
